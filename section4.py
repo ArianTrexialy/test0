8abdba458db4adbe2d7301458d99158be191171d
ArianTrexialy <arianalimohammadi4@gmail.com>
2023-08-04 00:54:47 -0700

    second commit

---
+++ section4.py
@@ -0,0 +1,25 @@
+'''m = "shanbe seperator 1shanbeseperator 2shanbe seperator3shanfgfrbe"
+
+def split_str(s,char):
+    start,end = 0,0
+    while end<len(s):
+        if m[end]==char or end==len(s)-1:
+            print(s[start:end])
+            start=end+1          
+        end+=1
+        
+split_str(m,"seperator")'''
+
+
+m = "seperatorshanbe seperator 1shanbeseperator 2shanbe seperator3shanfgfrbe"
+
+def split_str(s, char):
+    start = 0
+    end = 0
+    while end < len(s):
+        if s[end:end+len(char)] == char or end == len(s)-1:
+            print(s[start:end])
+            start = end + len(char)
+        end += 1
+
+split_str(m, "seperator")
